e1abc295655306cfbf81f2b2a1976cc676b548e3
zeng-liang <81370124+zeng-liang@users.noreply.github.com>
2026-02-12 16:58:45 +0800

    Convert Python group11 ID calculation to Java

--- shl.java
+++ shl.java
@@ -1,35 +1,29 @@
-def calculate_group_id():
-    import sys
-    input = sys.stdin.read().split()
-    n = int(input[0])
-    listPFR = list(map(int, input[1:n+1]))
-    
-    if n == 0:
-        print(0)
-        return
-    
-    right_max = [0] * n
-    right_min = [0] * n
-    
-    # 从右向左初始化最后一个元素
-    right_max[-1] = -float('inf')
-    right_min[-1] = float('inf')
-    
-    # 预处理右侧最大和最小值
-    for i in range(n-2, -1, -1):
-        right_max[i] = max(listPFR[i+1], right_max[i+1])
-        right_min[i] = min(listPFR[i+1], right_min[i+1])
-    
-    total = 0
-    for i in range(n):
-        current = listPFR[i]
-        if current >= right_max[i]:
-            total += 15
-        elif current < right_max[i] and current < right_min[i]:
-            total += 10
-        else:
-            total += 5
-    
-    print(total)
+import java.io.*;
+import java.util.Arrays;
 
-calculate_group_id()
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        int n = fs.nextInt();
+        int k = fs.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = fs.nextInt();
+
+        if (k > 0) Arrays.sort(a, 0, k);
+        if (k < n) {
+            Arrays.sort(a, k, n);
+            // reverse subarray [k, n)
+            for (int i = k, j = n - 1; i < j; i++, j--) {
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (i > 0) sb.append(' ');
+            sb.append(a[i]);
+        }
+        System.out.println(sb.toString());
+    }
